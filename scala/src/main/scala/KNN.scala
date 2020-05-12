037651349a6d003a0e7666e57deef780a202ea26
Ramtin R <ramtin@avlab.ai>
2020-05-11 20:58:44 -0700

    added data_prep for Scala

--- scala/src/main/scala/KNN.scala
+++ scala/src/main/scala/KNN.scala
@@ -23,7 +23,7 @@ object KNN {
   def str_column_to_int(dataset: ArrayBuffer[Array[Any]], column: Int): ArrayBuffer[Array[Any]] = {
     var class_values = for (row <- dataset) yield row(column)
     var unique = class_values.toSet
-    var lookup = new mutable.HashMap[Any, Int]
+    var lookup = new mutable.HashMap[Any, Float]
     for((v,i) <- unique.view.zipWithIndex){
       lookup(v) = i
     }
@@ -33,13 +33,52 @@ object KNN {
     return dataset
   }
 
+  def is_float(value: String): Boolean = {
+    try {
+      val num = value.toFloat
+      return true
+    }
+      catch
+    {
+      case e: NumberFormatException => return false
+    }
+  }
 
+  def data_normalizer(dataset: ArrayBuffer[Array[Any]]): ArrayBuffer[Array[Any]] = {
+    var max_list = new ArrayBuffer[Float]()
+    var min_list = new ArrayBuffer[Float]()
+    for(i <- 0 until dataset(0).length - 1){
+      var col = for(row <- dataset) yield row(i).asInstanceOf[Float]
+      min_list += col.min
+      max_list += col.max
+    }
 
+    for (row <- dataset){
+      for (col <- 0 until dataset(0).length - 1){
+        row(col) = (row(col).asInstanceOf[Float] - min_list(col))/
+                                            (max_list(col) - min_list(col))
+      }
+    }
+    return dataset
+  }
+
+  def data_prep(dataset: ArrayBuffer[Array[Any]]): ArrayBuffer[Array[Any]] = {
+    var new_dataset = dataset
+    for(col <- dataset(0).indices){
+      if (is_float(dataset(0)(col).toString)) {
+        new_dataset = str_column_to_float(dataset, col)
+      }
+      else {
+        new_dataset = str_column_to_int(dataset, col)
+      }
+    }
+    var final_data = data_normalizer(new_dataset)
+    return final_data
+  }
 
   def main(args:Array[String]){
     var dataset = load_csv("adult_short.csv")
-    dataset = str_column_to_float(dataset, 10)
-    str_column_to_int(dataset, 1)
+    dataset = data_prep(dataset)
     return
   }
 }
